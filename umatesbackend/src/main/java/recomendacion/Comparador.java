0847a94b1571d2c0c357ef7bb86f62c4df38efab
WILLIAM STIVEN DUARTE <ws.duarte@sis.virtual.uniandes.edu.co>
2018-04-17 23:02:28 -0500

    Creación del filtro completo y enumeración de criterios

--- umatesbackend/src/main/java/recomendacion/Comparador.java
+++ umatesbackend/src/main/java/recomendacion/Comparador.java
@@ -15,14 +15,43 @@ import java.util.List;
  * @author ws.duarte
  */
 public class Comparador {
+    
+    public static enum Criterio {
+        PROMEDIO("promedioEstimado","PROMEDIO"), 
+        DIFICULTAD("dificultadEstimada","DIFICULTAD"), 
+        CREDITOS("creditos","CREDITOS");
+        
+        private final String atributo;
+        private final String nombre;
+        Criterio(String atributo, String nombre) {
+            this.atributo = atributo;
+            this.nombre = nombre;
+        }
 
-    public static Comparator<MeteriaFiltro> filtrar(String... criterios) {
+        public String getAtributo() {
+            return atributo;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public static Criterio buscar(String nombre) {
+            for(Criterio c : Criterio.values()){
+                if(c.getNombre().equalsIgnoreCase(nombre))
+                    return c;
+            }
+            return null;
+        }
+    }
+
+    public static Comparator<MeteriaFiltro> filtrar(Criterio... criterios) {
         return (MeteriaFiltro o1, MeteriaFiltro o2) -> {
             try {
                 int ret = 0;
                 Field crit = null;
-                for (String string : criterios) {
-                    crit = MeteriaFiltro.class.getField(string);
+                for (Criterio string : criterios) {
+                    crit = MeteriaFiltro.class.getField(string.getAtributo());
                     ret = (int) crit.getType().getMethod("compareTo", crit.getType()).invoke(crit.get(o1), crit.get(o2));
                     if (ret != 0) {
                         break;
@@ -34,5 +63,7 @@ public class Comparador {
             }
         };
     }
+    
+    
 
 }
